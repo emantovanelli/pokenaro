6c5acb1f8918e60e252dc3ab409039bf4f1c3466
Ed Mantovanelli <ecmantovanelli@gmail.com>
2021-11-04 23:09:07 -0300

    adicionando imagens e bot

---
+++ bot.py
@@ -0,0 +1,109 @@
+import json
+import tweepy
+import requests
+from time import sleep
+from datetime import datetime, timezone, date, timedelta
+from os.path import exists
+
+
+def get_remainig_days():
+    diferenca = timedelta(hours=-3)
+    fuso_horario = timezone(diferenca)
+    date_with_timezone = datetime.now().astimezone(fuso_horario)
+    dia_salvacao = datetime.fromisoformat("2023-01-01").astimezone(fuso_horario)
+    diff = dia_salvacao - date_with_timezone
+    return diff.days
+
+
+def get_pokemon(pokemon_id):
+    request_pokemon = requests.get(
+        'https://pokeapi.co/api/v2/pokemon/{}'.format(pokemon_id))
+
+    qual_e_esse_pokemon = json.loads(request_pokemon._content)
+    return qual_e_esse_pokemon['name'].capitalize(), pokemon_id, qual_e_esse_pokemon['sprites']
+
+
+def make_tweet(api, dias, pokemon_name, pokemon_id, pokemon_sprite):
+    tweet_template = "O {} veio falar que faltam só mais {} dias para o Bolsonaro sair da presidência!\n\n #{} - {}"
+    try:
+        tweet = tweet_template.format(pokemon_name, dias, pokemon_name, pokemon_id)
+        media = api.media_upload(filename=pokemon_sprite, file=open(pokemon_sprite, 'rb'))
+        tweet_result = api.update_status(status=tweet, media_ids=[media.media_id])
+
+        print("Upload is done")
+    except Exception as e:
+        print("Error creating tweet")
+
+        print(e)
+
+
+def get_pokemons_sprite(quantity):
+    for i in range(quantity, 0, -1):
+        path = 'pokemons_image/' + str(i) + '.png'
+        file_exists = exists(path)
+        if not file_exists:
+            print('Baixando infos do pokemon: ' + str(i))
+            request_pokemon = requests.get(
+                'https://pokeapi.co/api/v2/pokemon/{}'.format(i))
+
+            qual_e_esse_pokemon = json.loads(request_pokemon._content)
+            print('Infos do {} baixadas'.format(qual_e_esse_pokemon['name']))
+
+            with open(path, 'wb') as handler:
+                print('Baixando imagem do {}'.format(qual_e_esse_pokemon['name']))
+
+                if qual_e_esse_pokemon['sprites']['other']['official-artwork']['front_default'] is not None:
+                    img_data = requests.get(
+                        qual_e_esse_pokemon['sprites']['other']['official-artwork']['front_default']).content
+                else:
+                    img_data = requests.get(qual_e_esse_pokemon['sprites']['front-default']).content
+                handler.write(img_data)
+                handler.close()
+                print('Imagem do {} baixada'.format(qual_e_esse_pokemon['name']))
+        sleep(1)
+
+
+def get_pokemon_sprite(pokemon_id):
+    path = 'pokemons_image/' + str(pokemon_id) + '.png'
+    file_exists = exists(path)
+    if not file_exists:
+        request_pokemon = requests.get(
+            'https://pokeapi.co/api/v2/pokemon/{}'.format(pokemon_id))
+        qual_e_esse_pokemon = json.loads(request_pokemon._content)
+        with open(path, 'wb') as handler:
+            img_data = requests.get(
+                qual_e_esse_pokemon['sprites']['other']['official-artwork']['front_default']).content
+            if img_data is None:
+                img_data = requests.get(qual_e_esse_pokemon['sprites']['front-default']).content
+            handler.write(img_data)
+            handler.close()
+    return path
+
+
+def main():
+    with open('config.json') as json_file:
+        data = json.load(json_file)
+        consumer_key = data['consumer_key']
+        consumer_secret = data['consumer_secret']
+        access_token = data['access_token']
+        access_token_secret = data['access_token_secret']
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    while False:
+        days = get_remainig_days()
+        pokemon = get_pokemon(days)
+
+        pokemon_sprite = get_pokemon_sprite(days)
+        print(pokemon[0])
+
+        make_tweet(api, days, pokemon[0], pokemon[1], pokemon_sprite)
+
+        sleep(86400)  # sleeps for 24 hours
+
+
+if __name__ == "__main__":
+    main()
